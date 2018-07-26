fb5dfc768f4e974ee4409291575ba6fbd10e87bf
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-07-26 15:06:49 +0900

    add_laravel5 make_layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>About</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <h1>About Me:{{ $first_name }} {{ $last_name }}</h1>
-</body>
-</html>
+@endsection
