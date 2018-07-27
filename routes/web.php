91f56d7d12012ed9e853f1e44436b4e5a261d48e
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-07-27 17:12:24 +0900

    add_laravel26

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('about', 'PagesController@about');
+Route::get('contact', 'PagesController@contact');
+Route::get('/', 'ArticlesController@index');
 Route::resource('articles', 'ArticlesController');
