fbed5abb4233cff52cb5ad3522524162328d3b5b
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-07-27 14:57:33 +0900

    add_laravel17

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -24,11 +24,17 @@ class ArticlesController extends Controller
       return view('articles.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+      $rules = [
+            'title' => 'required|min:3',
+            'body' => 'required',
+            'published_at' => 'required|date',
+      ];
+      $this->validate($request, $rules);
       $inputs = \Request::all();
-      Article::create($inputs);
+      Article::create($request->all());
       return redirect('articles');
-      
+
     }
 }
