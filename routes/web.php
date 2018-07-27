ac41a0e06c336ea3c276f28349048c8ae92daef9
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-07-27 18:21:12 +0900

    add_laravel29

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::post('auth/login','Auth\LoginController@login');
 Route::get('auth/logout','Auth\LoginController@logout');
 Route::get('auth/register','Auth\RegisterController@showRegistrationForm');
 Route::post('auth/register','Auth\RegisterController@register');
+Route::get('password/email', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@postReset')->name('password.reset');
