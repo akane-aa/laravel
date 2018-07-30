a5cd71231e029754a84b0ea2ac74b22b8bce7102
阿部茜 <abeakane@ibm-2dd9a2792d6.people.local>
2018-07-30 11:55:29 +0900

    add_laravel30

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,17 +4,21 @@
     <h1>Articles</h1>
 
     <hr/>
-    {!! link_to('articles/create','新規作成',['class' => 'btn btn-primary']) !!}
+    {{-- ログインしている時だけ表示 --}}
+    @if (Auth::check())
+      {!! link_to('articles/create', '新規作成', ['class' => 'btn btn-primary']) !!}
+    @endif
+
     @foreach($articles as $article)
-      <article>
-        <h2>
-            <a href="{{ url('articles',$article->id)}}">
-                {{$article->title}}
-            </a>
-        </h2>
-        <div class="body">
-          {{$article->body}}
-        </div>
-      </article>
+        <article>
+            <h2>
+                <a href="{{ url('articles', $article->id) }}">
+                    {{ $article->title }}
+                </a>
+            </h2>
+            <div class="body">
+                {{ $article->body }}
+            </div>
+        </article>
     @endforeach
 @stop
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,11 +8,28 @@
     <article>
         <div class="body">{{ $article->body }}</div>
     </article>
+
+    @unless ($article->tags->isEmpty())
+       <h5>Tags:</h5>
+       <ul>
+           @foreach($article->tags as $tag)
+               <li>{{ $tag->name }}</li>
+           @endforeach
+       </ul>
+   @endunless
+
+    {{-- ログインしている時だけ表示 --}}
+    @if (Auth::check())
     <br/>
 
     {!! link_to(action('ArticlesController@edit', [$article->id]), '編集', ['class' => 'btn btn-primary']) !!}
 
     <br/>
-        <br/>
-        {!! delete_form(['articles', $article->id]) !!}
+    <br/>
+
+    {!! delete_form(['articles', $article->id]) !!}
+    <!-- {!! Form::open(['method' => 'DELETE', 'url' => ['articles', $article->id]]) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!} -->
+    @endif
 @stop
